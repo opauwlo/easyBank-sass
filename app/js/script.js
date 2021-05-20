3226d889f01c39a5b3dbdce4f8484f85cd0dbd2b
opauwlo <opvcfreitas@gmail.com>
2021-05-19 22:48:30 -0300

    add menu and bug fix in icons change

--- app/js/script.js
+++ app/js/script.js
@@ -22,18 +22,28 @@ document.getElementById("themeButton").onclick = toggleTheme;
 
 // hamburguer menu 
 const mobileMenu = document.querySelector('#mobileMenu');
+const bodyHi = document.querySelector('body');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade');
 mobileMenu.addEventListener('click', function () {
   event.preventDefault();
   if (header.classList.contains('open')) {
+    bodyHi.classList.remove('noscroll');
     header.classList.remove('open');
-    overlay.classList.remove('fade-in');
-    overlay.classList.add('fade-out');
+    fadeElems.forEach(function(element){
+      element.classList.remove('fade-in');
+      element.classList.add('fade-out');
+
+    });
     
   } else {
     header.classList.add('open');
-    overlay.classList.remove('fade-out');
-    overlay.classList.add('fade-in');
+    bodyHi.classList.add('noscroll');
+    fadeElems.forEach(function(element){
+      element.classList.remove('fade-out');
+      element.classList.add('fade-in');
+
+    });
   }
 });
